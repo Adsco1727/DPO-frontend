6e5074de53689b3db516a7bc19f3bbf3cc74ab19
Intel Tech <jsreputation@gmail.com>
2022-04-26 22:39:59 +0800

    code style: added prettier

--- src/components/landing/whitepaper.jsx
+++ src/components/landing/whitepaper.jsx
@@ -1,10 +1,14 @@
 import MPrimaryBtn from "../../core/ui-kit/buttons/primaryBtn";
 
 export default function MWhitePaper() {
-
-    return (
-      <div className={'container pv-50 d-flex justify-content-center'}>
-        <MPrimaryBtn label={'WhitePaper'} key={'whitePaperRequest'} size={'large'} type={'primary'}/>
-      </div>
-    )
+  return (
+    <div className={"pv-50 d-flex justify-content-center"}>
+      <MPrimaryBtn
+        label={"WhitePaper"}
+        key={"whitePaperRequest"}
+        size={"large"}
+        type={"primary"}
+      />
+    </div>
+  );
 }
